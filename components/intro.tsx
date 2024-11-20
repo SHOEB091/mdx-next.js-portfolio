5d74053d7c76b862f92c259f7a69ed8c9f861b4f
shoeb <wiqbal091@gmail.com>
2024-11-20 20:22:16 +0530

    changes complete

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,16 +7,16 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hey, I&#39;m Shoeb.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software developer secialized in Backend developmnet , Cloud & DevOps. live in India. I&#39;m
+          I&#39;m a software developer specialized in Backend development, Cloud & DevOps. I live in India. I&#39;m
           passionate about learning new technologies and sharing knowledge with
           others.
         </p>
         <a 
-          href='/resume.pdf' 
-          download 
+          href='/shoeb_resume.pdf' 
+          target="_blank"
           className="mt-4 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none bg-primary text-primary-foreground shadow hover:bg-primary/90 h-8 px-3 py-1 w-full disabled:opacity-50"
         >
-          Download Resume
+          View Resume
         </a>
       </div>
       <div className='relative'>
